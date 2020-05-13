dd972a39ab4c193f7c0f9a3ae91d89d4a73e8162
gongjianbo <654344883@qq.com>
2020-05-13 22:38:01 +0800

    update MyPenStyle

--- MyPenColor.cpp
+++ MyPenColor.cpp
@@ -3,6 +3,7 @@
 #include <QPainter>
 #include <QtMath>
 #include <QLinearGradient>
+#include <QConicalGradient>
 #include <QTimer>
 
 MyPenColor::MyPenColor(QWidget *parent)
@@ -29,6 +30,7 @@ void MyPenColor::paintEvent(QPaintEvent *event)
     painter.setPen(QPen(Qt::white,1,Qt::DashLine));
     painter.drawRect(this->rect().adjusted(0,0,-1,-1)); //右下角会超出范围
 
+    //【1】sin正弦曲线
     //计算路径
     QPainterPath line_path;
     double angle=angleOffset;
@@ -82,4 +84,40 @@ void MyPenColor::paintEvent(QPaintEvent *event)
     for(int i:x_pos){
         painter.drawLine(i,0,i,height());
     }
+
+    //【2】雷达扫描
+    //黑底白框圆盘
+    const QRect scan_rect(10,10,100,100);//100*100
+    const int scan_radius=scan_rect.width()/2;
+    painter.save();
+    painter.translate(scan_rect.center());
+    QPainterPath ellipse_path;
+    ellipse_path.addEllipse(QPoint(0,0),scan_radius,scan_radius);
+    painter.fillPath(ellipse_path,Qt::black);
+    painter.setPen(QPen(Qt::white,2));
+    painter.setRenderHint(QPainter::Antialiasing,true);
+    painter.drawEllipse(QPoint(0,0),scan_radius,scan_radius);
+    //旋转扫描
+    QConicalGradient conical_gradient(QPoint(0,0),scan_radius);
+    conical_gradient.setAngle(-angleOffset*180.0);
+    conical_gradient.setColorAt(0.0,Qt::green);
+    conical_gradient.setColorAt(0.4,Qt::transparent);
+    conical_gradient.setColorAt(1.0,Qt::transparent);
+    painter.fillPath(ellipse_path,conical_gradient);
+    /*painter.setBrush(conical_gradient);
+    painter.setPen(Qt::NoPen);
+    painter.drawPie(QRect(-scan_radius,-scan_radius,2*scan_radius,2*scan_radius),
+                    -angleOffset*180.0*16,360*0.5*16);*/
+    //加一条线
+    painter.setPen(QPen(Qt::cyan,2));
+    painter.drawLine(QPoint(0,0),
+                     QPoint(scan_radius*cos(angleOffset*3.14159265),
+                            scan_radius*sin(angleOffset*3.14159265)
+                            ));
+    painter.setRenderHint(QPainter::Antialiasing,false);
+    //十字
+    painter.setPen(QPen(Qt::white,1,Qt::DashLine));
+    painter.drawLine(-scan_radius,0,scan_radius,0);
+    painter.drawLine(0,-scan_radius,0,scan_radius);
+    painter.restore();
 }
