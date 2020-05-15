6a7c201f2b31a3ee0dc489af4b428f5bcb2c80c2
gongjianbo <654344883@qq.com>
2020-05-15 21:06:20 +0800

    添加作者信息

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -10,6 +10,10 @@ MainWindow::MainWindow(QWidget *parent)
     ui->setupUi(this);
 
     setWindowTitle("Easy QPainter (By: GongJianBo 1992)");
+    qDebug()<<"Easy QPainter \n"
+              "展示 QPainter 的使用 \n"
+              "作者：龚建波 \n"
+              "QQ 交流群：647637553";
 }
 
 MainWindow::~MainWindow()
