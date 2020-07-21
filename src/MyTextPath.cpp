ae0a223182fa47bf1e98f0aa344be0c2950dc42f
gongjianbo <654344883@qq.com>
2020-07-21 23:36:07 +0800

    增加横向滚动的文字

--- src/MyTextPath.cpp
+++ src/MyTextPath.cpp
@@ -1,5 +1,9 @@
 #include "MyTextPath.h"
 
+#include <QTimerEvent>
+#include <QPaintEvent>
+#include <QResizeEvent>
+
 #include <QPainter>
 #include <QPainterPath>
 #include <QLinearGradient>
@@ -8,26 +12,25 @@
 MyTextPath::MyTextPath(QWidget *parent)
     : QWidget(parent)
 {
-
+    //定时移动
+    startTimer(50);
 }
 
 void MyTextPath::paintEvent(QPaintEvent *event)
 {
-    Q_UNUSED(event)
+    event->accept();
     QPainter painter(this);
     //先画一个白底黑框
     painter.fillRect(this->rect(),Qt::white);
     QPen pen(Qt::black);
     painter.setPen(pen);
     painter.drawRect(this->rect().adjusted(0,0,-1,-1)); //右下角会超出范围
-
-    //最短边
+    //窗口最短边
     const int border_width=width()>height()?height():width();
-    //painter.save();
 
     //中心绘制一个Qt文字
     QColor qt_color(200,250,200); //淡淡的原谅色
-    QFont qt_font("Microsoft YaHei",border_width/2);
+    QFont qt_font("Microsoft YaHei",border_width/3);
     QPen qt_pen(qt_color,border_width/20+1);
     painter.setFont(qt_font);
     painter.setPen(qt_pen);
@@ -54,7 +57,6 @@ void MyTextPath::paintEvent(QPaintEvent *event)
     painter.setRenderHint(QPainter::Antialiasing);
     painter.drawPath(qt_str_path);
     painter.drawRect(qt_rect);
-    //painter.restore();
 
     //左上角绘制文字
     const QString str_1="一切都是没有结局的开始";
@@ -65,11 +67,9 @@ void MyTextPath::paintEvent(QPaintEvent *event)
     //暂时用的固定值坐标
     str_path.addText(20,str_height,str_font,str_1);
     str_path.addText(20,str_height*2,str_font,str_2);
-
     painter.setPen(QPen(Qt::green,0.5));
-
-    QLinearGradient str_gradient(0,0,
-                                 0,str_height*2);
+    QLinearGradient str_gradient(0,0,0,str_height*2);
+    //用渐变色来实现文字填充一半的效果，中间部分填充透明色
     //百分比用值计算好点，我这里手动调的固定值
     str_gradient.setColorAt(0.0,Qt::green);
     str_gradient.setColorAt(0.35,Qt::green);
@@ -79,4 +79,77 @@ void MyTextPath::paintEvent(QPaintEvent *event)
     str_gradient.setColorAt(1.0,Qt::green);
     painter.drawPath(str_path);
     painter.fillPath(str_path,str_gradient);
+
+    //底部滚动的文字1
+    painter.setPen(QPen(Qt::red,2));
+    QFont scroll_font("Microsoft YaHei",20);
+    painter.setFont(scroll_font);
+    const QString scroll_text0="你一会儿看我，一会儿看云。";
+    QString scroll_text1;
+    for(auto riter=scroll_text0.rbegin();riter!=scroll_text0.rend();riter++)
+        scroll_text1.push_back(*riter);
+    const int scroll_width1 = painter.fontMetrics().width(scroll_text1);
+    const int scroll_height1 = painter.fontMetrics().height();
+    const int scroll_y1 = height()-scroll_height1-10; //Qt文本起点在左下角
+    //文本宽度改变，重新滚动(包括设置了文本，修改了字体大小等)
+    if (textWidth_1 != scroll_width1 && scroll_width1 > 0) {
+        textWidth_1 = scroll_width1;
+        textOffset_1 = 0;
+    }
+    else {
+        painter.drawText(textOffset_1-textWidth_1, scroll_y1, scroll_text1);
+    }
+
+    //底部滚动的文字2
+    const QString scroll_text2="我觉得你看我时很远，你看云时很近。";
+    const int scroll_width2 = painter.fontMetrics().width(scroll_text2);
+    //const int scroll_height2 = painter.fontMetrics().capHeight();
+    const int scroll_y2 = height()-10; //Qt文本起点在左下角
+    //文本宽度改变，重新滚动(包括设置了文本，修改了字体大小等)
+    if (textWidth_2 != scroll_width2 && scroll_width2 > 0) {
+        textWidth_2 = scroll_width2;
+        textOffset_2 = 0;
+    }
+    else {
+        painter.drawText(labelWidth_2 - textOffset_2, scroll_y2, scroll_text2);
+    }
+}
+
+void MyTextPath::timerEvent(QTimerEvent *event)
+{
+    event->accept();
+    //减小定时间隔和步进可以更平滑，但更费cpu
+
+    //文本从左往右循环滚动
+    textOffset_1+=3;
+    if (textOffset_1 > textWidth_1 + labelWidth_1) {
+        textOffset_1 = 0;
+    }
+    //文本从右往左循环滚动
+    textOffset_2+=3;
+    if (textOffset_2 > textWidth_2 + labelWidth_2) {
+        textOffset_2 = 0;
+    }
+    update();
+}
+
+void MyTextPath::resizeEvent(QResizeEvent *event)
+{
+    const int old_width = event->oldSize().width();
+    const int new_width = event->size().width();
+    //从左往右，比之前的更小，重新滚动
+    if (new_width > 10) {
+        labelWidth_1 = new_width;
+        if (new_width < old_width) {
+            textOffset_1 = 0;
+        }
+    }
+    //从右往左，比之前的更小，重新滚动
+    if (new_width > 10) {
+        labelWidth_2 = new_width;
+        if (new_width < old_width) {
+            textOffset_2 = 0;
+        }
+    }
+    QWidget::resizeEvent(event);
 }
--- src/MyTextPath.h
+++ src/MyTextPath.h
@@ -11,7 +11,21 @@ public:
     explicit MyTextPath(QWidget *parent = nullptr);
 
 protected:
+    //绘图
     void paintEvent(QPaintEvent *event) override;
+    //定时器
+    void timerEvent(QTimerEvent * event) override;
+    //窗口改变大小，可能需要重新计算
+    void resizeEvent(QResizeEvent * event) override;
+
+private:
+    int textOffset_1=0;//文本移动偏移量
+    int textWidth_1=1;//文本绘制宽度
+    int labelWidth_1=1;//文本绘制区域宽度
+
+    int textOffset_2=0;//文本移动偏移量
+    int textWidth_2=1;//文本绘制宽度
+    int labelWidth_2=1;//文本绘制区域宽度
 };
 
 #endif // MYTEXTPATH_H
