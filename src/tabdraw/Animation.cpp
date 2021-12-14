5c97ba0127176c7bf9eaccacb199eac13c4f413b
gongjianbo <654344883@qq.com>
2021-12-14 23:54:58 +0800

    色块属性动画

--- src/tabdraw/Animation.cpp
+++ src/tabdraw/Animation.cpp
@@ -4,28 +4,82 @@
 #include <QPaintEvent>
 #include <QPainter>
 #include <QGradient>
+#include <QDebug>
 
 Animation::Animation(QWidget *parent)
     : QWidget(parent)
 {
-    //1-1.波纹
+    // 1-1.波纹
     connect(&waveTimer, &QTimer::timeout, this, [this]
     {
         waveOffset += 4;
         waveOffset %= waveLimit;
         update();
     });
+    // 2-1.色块
+    blockAnimation = new QParallelAnimationGroup(this);
+    QPropertyAnimation *block_color = new QPropertyAnimation(this);
+    block_color->setTargetObject(this);
+    block_color->setPropertyName("blockColor");
+    block_color->setStartValue(QColor(255, 0, 0));
+    block_color->setEndValue(QColor(255, 255, 0));
+    block_color->setDuration(3000);
+    blockAnimation->addAnimation(block_color);
+    QPropertyAnimation *block_scare = new QPropertyAnimation(this);
+    block_scare->setTargetObject(this);
+    block_scare->setPropertyName("blockScare");
+    block_scare->setStartValue(1.0f);
+    block_scare->setEndValue(0.2f);
+    block_scare->setEasingCurve(QEasingCurve::InCubic); //缓和曲线设置
+    block_scare->setDuration(2000);
+    blockAnimation->addAnimation(block_scare);
+    //播放完了就倒着播放
+    connect(blockAnimation, &QParallelAnimationGroup::finished, this, [this]
+    {
+        if(blockAnimation->direction() == QAbstractAnimation::Forward){
+            blockAnimation->setDirection(QAbstractAnimation::Backward);
+        }else{
+            blockAnimation->setDirection(QAbstractAnimation::Forward);
+        }
+        blockAnimation->start();
+    });
+}
+
+QColor Animation::getBlockColor() const
+{
+    return blockColor;
+}
+
+void Animation::setBlockColor(const QColor &color)
+{
+    blockColor = color;
+    emit blockColorChanged();
+    update();
+}
+
+float Animation::getBlockScare() const
+{
+    return blockScare;
+}
+
+void Animation::setBlockScare(float scare)
+{
+    blockScare = scare;
+    emit blockScareChanged();
+    update();
 }
 
 void Animation::showEvent(QShowEvent *event)
 {
     waveTimer.start(50);
+    blockAnimation->start();
     QWidget::showEvent(event);
 }
 
 void Animation::hideEvent(QHideEvent *event)
 {
     waveTimer.stop();
+    blockAnimation->pause();
     QWidget::hideEvent(event);
 }
 
@@ -37,16 +91,31 @@ void Animation::paintEvent(QPaintEvent *event)
     painter.fillRect(this->rect(), Qt::black);
     painter.setPen(Qt::white);
 
-    //1-1.波纹-圆心(200,200)，半径(100)
-    QRect wave_rect(100, 100, 200, 200);
-    QRadialGradient gradient(wave_rect.center(), 100);
-    const double f_space = 0.18;                                      //波纹间隔，总量为1
-    const double f_inner = 0.05;                                      //一层波纹的内侧偏移，相对于f_offset
-    const double f_crest = 0.03;                                      //波峰偏移，相对于f_offset
-    const double f_outer = 0.06;                                      //波纹的外侧偏移，相对于f_offset
-    const double f_offset = waveOffset / double(waveLimit) * f_space; //当前波纹偏移
-    const int f_count = std::floor(1 / f_space);                      //波纹个数
-    QColor wave_color(255, 0, 0);                                     //波峰颜色
+    // 1-1.波纹
+    drawWave(painter, QRect(20, 20, 200, 200));
+    // 2-1.色块
+    drawColorBlock(painter, QRect(240, 20, 200, 200));
+}
+
+void Animation::drawWave(QPainter &painter, const QRectF &area)
+{
+    painter.save();
+    //波纹
+    QRadialGradient gradient(area.center(), 100);
+    //波纹间隔，总量为1
+    const double f_space = 0.18;
+    //一层波纹的内侧偏移，相对于f_offset
+    const double f_inner = 0.02;
+    //波峰偏移，相对于f_offset
+    const double f_crest = 0.03;
+    //波纹的外侧偏移，相对于f_offset
+    const double f_outer = 0.06;
+    //当前波纹偏移
+    const double f_offset = waveOffset / double(waveLimit) * f_space;
+    //波纹个数
+    const int f_count = std::floor(1 / f_space);
+    //波峰颜色
+    QColor wave_color(255, 0, 0);
     gradient.setColorAt(0, Qt::transparent);
     for (int i = 0; i < f_count; i++)
     {
@@ -69,8 +138,24 @@ void Animation::paintEvent(QPaintEvent *event)
     }
     //波纹抗锯齿
     painter.setRenderHint(QPainter::Antialiasing, true);
-    painter.fillRect(wave_rect, gradient);
-    //画直线不用抗锯齿
+    painter.fillRect(area, gradient);
+    //边框，画直线不用抗锯齿
     painter.setRenderHint(QPainter::Antialiasing, false);
-    painter.drawRect(wave_rect);
+    painter.drawRect(area);
+    painter.restore();
+}
+
+void Animation::drawColorBlock(QPainter &painter, const QRectF &area)
+{
+    painter.save();
+    QRectF block = area.adjusted(10, 10, -10, -10);
+    block.setWidth(block.width() * getBlockScare());
+    block.setHeight(block.height() * getBlockScare());
+    block.moveCenter(area.center());
+    painter.setRenderHint(QPainter::Antialiasing, true);
+    painter.fillRect(block, getBlockColor());
+    //边框
+    painter.setRenderHint(QPainter::Antialiasing, false);
+    painter.drawRect(area);
+    painter.restore();
 }
--- src/tabdraw/Animation.h
+++ src/tabdraw/Animation.h
@@ -2,13 +2,24 @@
 #include <QWidget>
 #include <QPainterPath>
 #include <QTimer>
+#include <QPropertyAnimation>
+#include <QParallelAnimationGroup>
+#include <QSequentialAnimationGroup>
 
 //动画效果
 class Animation : public QWidget
 {
     Q_OBJECT
+    Q_PROPERTY(QColor blockColor READ getBlockColor WRITE setBlockColor NOTIFY blockColorChanged)
+    Q_PROPERTY(float blockScare READ getBlockScare WRITE setBlockScare NOTIFY blockScareChanged)
 public:
     explicit Animation(QWidget *parent = nullptr);
+
+    QColor getBlockColor() const;
+    void setBlockColor(const QColor &color);
+
+    float getBlockScare() const;
+    void setBlockScare(float scare);
 
 protected:
     //显示时才启动定时动画
@@ -18,9 +29,25 @@ protected:
     void paintEvent(QPaintEvent *event) override;
 
 private:
-    //1.定时器动画
-    //1-1.波纹
+    //定时器波纹
+    void drawWave(QPainter &painter, const QRectF &area);
+    //属性动画色块
+    void drawColorBlock(QPainter &painter, const QRectF &area);
+
+signals:
+    void blockColorChanged();
+    void blockScareChanged();
+
+private:
+    // 1.定时器动画
+    // 1-1.波纹
     QTimer waveTimer;
     int waveOffset{0};
     const int waveLimit{100};
+    // 2.属性动画
+    // 2-1.色块
+    //一个并行执行的动画组，同时改变颜色和大小
+    QParallelAnimationGroup *blockAnimation{nullptr};
+    QColor blockColor{255, 0, 0};
+    float blockScare{1.0};
 };
